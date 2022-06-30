07bb8f5df32a44e9e138fb7a0e3f6e9a617f97f2
Shubham Vyavahare <33980269+ShubhamVyavahare@users.noreply.github.com>
2022-06-30 12:53:38 +0530

    Implemented Demo 16

--- src/scenariobased/demo16/Demo16.java
+++ src/scenariobased/demo16/Demo16.java
@@ -11,7 +11,7 @@ public class Demo16 {
 
     public static void main(String[] args) {
 
-        String str = "my name is shubham ";
+        String str = "my name is shubham";
         method1(str);
         method2(str);
     }
@@ -22,7 +22,7 @@ public class Demo16 {
         Stack<Character> characterStack = new Stack<>();
 
         for (int i = 0; i < chars.length; i++) {
-            if (!Character.isSpaceChar(chars[i])) {
+            if (Character.isAlphabetic(chars[i])) {
                 characterStack.push(chars[i]);
             } else {
                 while (!characterStack.empty()) {
@@ -30,6 +30,10 @@ public class Demo16 {
                 }
                 System.out.print(" ");
             }
+        }
+
+        while (!characterStack.empty()) {
+            System.out.print(characterStack.pop());
         }
     }
 
